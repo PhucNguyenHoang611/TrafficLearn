a173f20c45f13e4309dceb10a681d400c1372968
Sang <75106540+sang1833@users.noreply.github.com>
2023-10-24 11:13:08 +0700

    add library

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,59 +1,20 @@
-import React, { Component } from 'react';
+import React from "react";
 
-export default class App extends Component {
-    static displayName = App.name;
+const App = () => {
+  return (
+    <>
+      <div className="text-red-700">test</div>
+      <div className="bg-black">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+        mollitia eius accusamus. Aperiam laborum at saepe beatae aliquid harum
+        repellendus corrupti hic inventore libero alias debitis, ullam delectus
+        eos quod. Obcaecati deserunt nisi illum asperiores? Illo at, possimus
+        soluta nisi non, quod, quis saepe cumque voluptatem quae corrupti
+        mollitia corporis. Minima non totam dignissimos nesciunt corrupti quae
+        itaque fugiat ab.
+      </div>
+    </>
+  );
+};
 
-    constructor(props) {
-        super(props);
-        this.state = { forecasts: [], loading: true };
-    }
-
-    componentDidMount() {
-        this.populateWeatherData();
-    }
-
-    static renderForecastsTable(forecasts) {
-        return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {forecasts.map(forecast =>
-                        <tr key={forecast.date}>
-                            <td>{forecast.date}</td>
-                            <td>{forecast.temperatureC}</td>
-                            <td>{forecast.temperatureF}</td>
-                            <td>{forecast.summary}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    }
-
-    render() {
-        let contents = this.state.loading
-            ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-            : App.renderForecastsTable(this.state.forecasts);
-
-        return (
-            <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                {contents}
-            </div>
-        );
-    }
-
-    async populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
-    }
-}
+export default App;
---
+++ reactapp/tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+/*eslint-env node*/
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      transitionProperty: {
+        width: "width",
+        height: "height",
+      },
+      colors: {
+        "primary-0": "#423A8E",
+        "primary-1": "#00CCCD",
+        "secondary-0": "#FFC107",
+        "secondary-1": "#DC3545",
+        "secondary-2": "#198754",
+        "secondary-3": "#0D6EFD",
+      },
+      screens: {
+        phone: "444px",
+        // => @media (min-width: 640px) { ... }
+
+        "phone-1": "516px",
+        // => @media (min-width: 640px) { ... }
+
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
